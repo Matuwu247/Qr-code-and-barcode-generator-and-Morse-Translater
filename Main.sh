eea21c7a0177b20bdab01db0c8af195daa568cc4
Matuwu247 <148290987+Matuwu247@users.noreply.github.com>
2023-11-02 17:59:36 -0300

    Codigo optimized
    Se saco la compilacion de cada caso ;)

--- Main.sh
+++ Main.sh
@@ -1,37 +1,23 @@
 #!/bin/bash
-echo -e "\n\t\t\t\t\t\tCargando su Codigo QR personalizado"
-barra()
+exportvar()
 {
-echo -e "\v"
-seconds=0,08
-clear
-for i in {1..50..1}
-do
-   echo -n "███"   
-   sleep $seconds
-done
-echo -e "\n\t\t\t\t\t\tQR generado, presione ENTER para continuar"
+    	shift 1
+        arg=$@
+        export ARGUMENTO="$arg"
 }
 case ${1,,} in
     "-q")
     	shift 1
         arg=$@
         export ARGUMENTO="$arg"
-	barra & pid1=$!
-	g++ -Wall Qrcode.cpp qrcodegen.cpp qrcodegen.hpp -o salida.out & pid2=$!
-	wait $pid1
-	wait $pid2
-	read a
-	./salida.out
+    	./salidaQ.out
 	
     ;; 
-
     "-m") 
         shift 1
         arg=$@
         export ARGUMENTO="$arg"
-        g++ MorseTranslator.cpp -o salida.out
-        ./salida.out
+        ./salidaM.out
         
      ;;
     "-h" | "--help")
@@ -44,18 +30,15 @@ case ${1,,} in
     decir a continuacion del -m o -q. Es importante que este entre comillas porque sino no funcionara."
     ;;
     "-qm") 
-        shift 1
+	shift 1
         arg=$@
         export ARGUMENTO="$arg"
-	g++ -Wall Qrcode.cpp qrcodegen.cpp qrcodegen.hpp -o salida.out
-	./salida.out
-        g++ MorseTranslator.cpp -o salida.out
-        ./salida.out
+        ./salidaQ.out
+        ./salidaM.out
      ;;
     *)
         echo "Argumento equivocado"
     ;;
 esac
-
 echo -e "\n\t\t\t\t\t\tPrograma Terminado, presione ENTER para finalizar\v"
 read a
