b18a5acd3e4fda249a8703b46b2c6e2fae1fbece
Matuwu247 <148290987+Matuwu247@users.noreply.github.com>
2023-11-02 10:02:44 -0300

    Script menu
    a script menu with arguments to select QR or Morse

---
+++ Main.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+case $1 in
+    "-q" | "-Q")
+        arg=$2
+        export ARGUMENTO="$arg"
+	g++ -Wall Codigoqr.cpp qrcodegen.cpp qrcodegen.hpp -o salida.out
+	./salida.out
+    
+    ;; 
+
+    "-m" | "-M") 
+        arg=$2
+        export ARGUMENTO="$arg"
+        g++ morse.cpp -o salida.out
+        ./salida.out
+        
+     ;;
+    "-h" | "-H" | "--help" | "--Help"| "--HELP")
+        echo "Buenas usuario el codigo funciona de la siguiente manera:
+    Primero deberemos de colocar que tipo de transformacion queremos. Esto lo haremos poniendo los 
+    siguientes parametros:
+    -q o -Q: Este parametro es la transformacion del texto ingresado a codigo QR
+    -m o -M: Este parametro es la transformacion del texto ingresado a codigo Morse
+    Luego para el texto a traducir debemos colocar unas comillas a continuacion del primer parametro es 
+    decir a continuacion del -m o -q. Es importante que este entre comillas porque sino no funcionara."
+
+    ;;
+    *)
+        echo "Argumento equivocado"
+    ;;
+esac
+
+
