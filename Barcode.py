f986db14487896b8d85f53835eeb1a80cea6204a
Matuwu247 <148290987+Matuwu247@users.noreply.github.com>
2023-11-08 17:53:49 -0300

    fix the character at least ;)

--- Barcode.py
+++ Barcode.py
@@ -3,8 +3,6 @@ from barcode import Code39
 from barcode.writer import ImageWriter
 import os
 texto = os.environ["ARGUMENTO"]
-
-code = Code39(texto, writer=ImageWriter())
-
+code = Code39(texto, writer=ImageWriter(),add_checksum=False)
 barcode_image = code.render()
 barcode_image.show()
