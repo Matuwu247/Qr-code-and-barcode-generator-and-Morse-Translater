dc062e3c0ab643ec1f64af54f2fb05e5fa0d8ab3
Matuwu247 <148290987+Matuwu247@users.noreply.github.com>
2023-11-09 10:04:24 -0300

    comments added

--- Barcode.py
+++ Barcode.py
@@ -1,8 +1,8 @@
 import barcode
 from barcode import Code39
 from barcode.writer import ImageWriter
-import os
-texto = os.environ["ARGUMENTO"]
-code = Code39(texto, writer=ImageWriter(),add_checksum=False)
-barcode_image = code.render()
-barcode_image.show()
+import os // Importa la variable de entorno desde el bash
+texto = os.environ["ARGUMENTO"] 
+code = Code39(texto, writer=ImageWriter(),add_checksum=False) // Usa la funcion y declara que codificacion de barras utilizara
+barcode_image = code.render() //renderiza la informacion de la variable enviada
+barcode_image.show() // Convierte, hace y muestra el archivo temporal con el codigo de barras
