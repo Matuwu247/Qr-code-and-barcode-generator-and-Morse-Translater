ae536929da0347a1c633dcbc11bd3f4275020883
Matuwu247 <148290987+Matuwu247@users.noreply.github.com>
2023-11-09 10:17:31 -0300

    Comments added

--- Main.sh
+++ Main.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-case ${1,,} in
-    "-q")
-    	shift 1
-        arg=$@
-        export ARGUMENTO="$arg"
-    	./salidaQ.out
+case ${1,,} in #iniciams el switch y la ,, hace la funcion de convertir todo a minuscula
+    "-q") #Caso de codigo QR
+    	shift 1 #Mueve el argumento
+        arg=$@ #Esto tomara todo lo ingresado luego del primer argumento
+        export ARGUMENTO="$arg" #Tomas lo ingresado en el primer parametro y lo envia al codigo
+    	./salidaQ.out #Ejecuta el codigo de c++ compilado 
 	
     ;; 
-    "-m") 
+    "-m") #Caso de codigo Morse
         shift 1
         arg=$@
         export ARGUMENTO="$arg"
         ./salidaM.out
         
      ;;
-     "-b")
+     "-b") #Caso de codigo de barra
 	shift 1
         arg=$@
         export ARGUMENTO="$arg"
-	python3 Barcode.py
+	python3 Barcode.py #Ejecuta el interprete de python
      ;;
-         "-qm") 
+         "-qm") #Caso de codigo QR y Codigo Morse
 	shift 1
         arg=$@
         export ARGUMENTO="$arg"
         ./salidaQ.out
         ./salidaM.out
      ;;
-    "-h" | "--help")
+    "-h" | "--help") #En caso de necesitar ayuda explica el funcionamiento
         echo "Buenas usuario el codigo funciona de la siguiente manera:
     Primero deberemos de colocar que tipo de transformacion queremos. Esto lo haremos poniendo los 
     siguientes parametros:
@@ -38,7 +38,7 @@ case ${1,,} in
     decir a continuacion del -m o -q. Es importante que este entre comillas porque sino no funcionara."
     ;;
 
-    *)
+    *) // toma todos los casos que no estan mencionados arriba
         echo "Argumento equivocado"
     ;;
 esac
